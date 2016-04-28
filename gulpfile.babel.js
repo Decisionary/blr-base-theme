e2efbd2bcaed75b447915a102bd5aabac36647aa
Wes Moberly <wesm87@gmail.com>
2016-04-28 14:45:15 -0400

    Switch to ESLint, update Grid settings and menu item styles

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,6 @@
-'use strict';
+
+// Babel
+import 'babel-polyfill';
 
 // Gulp
 import gulp         from 'gulp';
@@ -16,20 +18,26 @@ import autoprefixer from 'gulp-autoprefixer';
 // JS
 import babel        from 'gulp-babel';
 import uglify       from 'gulp-uglify';
-import jshint       from 'gulp-jshint';
 
+/**
+ * Gulp tasks.
+ */
 class GulpTasks {
 
+	/**
+	 * Task configuration.
+	 *
+	 * @return {Object} The task configuration.
+	 */
 	get config() {
 		return {
 			sass: {
-				outputStyle: 'expanded',
-				precision: 10,
+				outputStyle:  'expanded',
+				precision:    10,
 				includePaths: [
 					'bower_components',
-					'node_modules'
+					'node_modules',
 				],
-				errorLogToConsole: true,
 			},
 			autoprefixer: {
 				browsers: [
@@ -39,55 +47,79 @@ class GulpTasks {
 					'ie > 8',
 					'> 1%',
 				],
-			}
+			},
 		};
 	}
 
+	/**
+	 * Files paths.
+	 *
+	 * @return {Object} File paths.
+	 */
 	get files() {
 		return {
 			css: {
+				watch:  'assets/source/css/**/*.scss',
 				source: [
 					'assets/source/css/**/*.scss',
 					'!assets/source/css/**/_*.scss',
 				],
-				dest:   'assets/dist/css/'
+				dest: 'assets/dist/css/',
 			},
 			js: {
+				watch:  'assets/source/js/**/*.js',
 				source: 'assets/source/js/**/*.js',
 				dest:   'assets/dist/js',
 			},
 		};
 	}
 
+	/**
+	 * Tasks.
+	 *
+	 * @return {Array} Tasks.
+	 */
 	get tasks() {
 		return [
 			'build',
 			'buildCSS',
 			'buildJS',
 			'watch',
-			'default'
+			'default',
 		];
 	}
 
+	/**
+	 * Constructor.
+	 *
+	 * Once ES7 arrives we can just define class methods as arrow functions.
+	 * In the meantime, we need to bind each task method to the class.
+	 */
 	constructor() {
-
-		// Once ES7 arrives we can just define class methods as arrow functions.
-		// In the meantime, we need to bind each task method to the class.
-		this.tasks.forEach(( task ) => {
+		this.tasks.forEach( task => {
 			this[ task ] = this[ task ].bind( this );
 			gulp.task( task, this[ task ] );
 		});
 	}
 
-	build( done ) {
-		this.buildCSS();
-		this.buildJS();
+	/**
+	 * Build task.
+	 *
+	 * @param  {Function} done Async callback.
+	 */
+	async build( done ) {
+		await this.buildCSS();
+		await this.buildJS();
 		done();
 	}
 
+	/**
+	 * CSS build task.
+	 *
+	 * @return {Function}
+	 */
 	buildCSS() {
-		return gulp
-			.src( this.files.css.source )
+		return gulp.src( this.files.css.source )
 			.pipe( sourcemaps.init() )
 			.pipe( sass( this.config.sass ) )
 			.pipe( concat( 'app.css' ) )
@@ -99,9 +131,13 @@ class GulpTasks {
 			.pipe( gulp.dest( this.files.css.dest ) );
 	}
 
+	/**
+	 * JS build task.
+	 *
+	 * @return {Function}
+	 */
 	buildJS() {
-		return gulp
-			.src( this.files.js.source )
+		return gulp.src( this.files.js.source )
 			.pipe( sourcemaps.init() )
 			.pipe( babel() )
 			.pipe( concat( 'app.js' ) )
@@ -110,16 +146,23 @@ class GulpTasks {
 			.pipe( rename({ suffix: '.min' }) )
 			.pipe( sourcemaps.write( './maps' ) )
 			.pipe( gulp.dest( this.files.js.dest ) );
-
 	}
 
+	/**
+	 * Watch task.
+	 */
 	watch() {
-		gulp.watch( this.files.js.source, this.buildJS );
-		gulp.watch( this.files.css.source[0], this.buildCSS );
+		gulp.watch( this.files.js.watch, this.buildJS );
+		gulp.watch( this.files.css.watch, this.buildCSS );
 	}
 
+	/**
+	 * Default task.
+	 *
+	 * @param  {Function} done Async callback.
+	 */
 	default( done ) {
-		return this.build( done );
+		this.build( done );
 	}
 }
 
