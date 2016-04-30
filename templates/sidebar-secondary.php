bf3a585642f0d81c03719a1154baf9b586ba5f0a
Wes Moberly <wesm87@gmail.com>
2016-04-29 21:58:48 -0400

    Add placeholder White Papers widget

--- templates/sidebar-secondary.php
+++ templates/sidebar-secondary.php
@@ -7,4 +7,15 @@
 
 ?>
 
+<section class="widget widget--white-papers">
+	<h3 class="widget__title">FREE White Papers</h3>
+
+	<div class="widget__content entry__content">
+		<p><a href="#">White Paper Placeholder Text</a></p>
+		<p><a href="#">White Paper Placeholder Text</a></p>
+		<p><a href="#">White Paper Placeholder Text</a></p>
+		<p><a href="#">White Paper Placeholder Text</a></p>
+	</div>
+</section>
+
 <?php dynamic_sidebar( 'sidebar-secondary' ); ?>
