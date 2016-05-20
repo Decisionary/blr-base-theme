9658975b0842655261e60d7e265a59098deec91e
Jack Smith <jack.smith@decisonarytech.com>
2016-05-20 11:49:53 -0600

    BLR-170: Aligned header search and right logo

--- templates/header.php
+++ templates/header.php
@@ -43,53 +43,54 @@ $phone_number = get_theme_mod( 'blr_base_theme_phone_number' );
 	<?php endif; ?>
 
 </div><!-- /.branding -->
-
-<div class="search search--header">
-
-	<div class="site-info site-info--header" role="contentinfo">
-
+<div class="header--right-side" role="contentinfo">
+	<div class="header--row header--top-row">
+		<!--Phone Number-->
 		<?php if ( ! empty( $phone_number ) ) : ?>
 			<p class="site-info__phone-number">
 				<?php echo wp_kses_post( $phone_number ); ?>
 			</p>
 		<?php endif; ?>
-
+		<!--Social Icons-->
 		<?php if ( class_exists( 'ET_Monarch' ) && is_callable( [ ET_Monarch, 'display_widget' ] ) ) : ?>
 			<span class="site-info__social-icons">
-				<?php ET_Monarch::display_widget( 'shortcode' ); ?>
-			</span>
+			<?php ET_Monarch::display_widget( 'shortcode' ); ?>
+		</span>
 		<?php endif; ?>
-
-		<?php if ( has_nav_menu( 'nav-search' ) ) : ?>
-			<nav class="nav nav--search">
-				<?php wp_nav_menu( [ 'theme_location' => 'nav-search' ] ); ?>
-			</nav><!-- /.nav -->
-		<?php endif; ?>
-
 	</div>
+	<div class="header--row">
+		<!--Search-->
+		<div class="header--search">
+			<?php if ( has_nav_menu( 'nav-search' ) ) : ?>
+				<nav class="nav nav--search">
+					<?php wp_nav_menu( [ 'theme_location' => 'nav-search' ] ); ?>
+				</nav><!-- /.nav -->
+			<?php endif; ?>
+			<?php echo get_search_form(); ?>
+		</div>
+		<!--Right logo-->
+		<div class="header--right-logo">
+			<?php if ( ! empty( $logo_search_image_url ) ) : ?>
 
-	<?php echo get_search_form(); ?>
+				<?php if ( ! empty( $logo_search_link_url ) ) : ?>
+					<a class="logo logo--search" href="<?php echo esc_url( $logo_search_link_url ); ?>">
+				<?php else : ?>
+					<div class="logo logo--search">
+				<?php endif; ?>
 
-</div><!-- /.search -->
+				<img
+					class="logo__image"
+					src="<?php echo esc_url( $logo_search_image_url ); ?>"
+					alt="<?php echo esc_attr( $logo_search_alt_text ); ?>"
+				>
 
-<?php if ( ! empty( $logo_search_image_url ) ) : ?>
+				<?php if ( ! empty( $logo_search_link_url ) ) : ?>
+					</a><!-- /.logo -->
+				<?php else : ?>
+					</div><!-- /.logo -->
+				<?php endif; ?>
 
-	<?php if ( ! empty( $logo_search_link_url ) ) : ?>
-		<a class="logo logo--search" href="<?php echo esc_url( $logo_search_link_url ); ?>">
-	<?php else : ?>
-		<div class="logo logo--search">
-	<?php endif; ?>
-
-	<img
-		class="logo__image"
-		src="<?php echo esc_url( $logo_search_image_url ); ?>"
-		alt="<?php echo esc_attr( $logo_search_alt_text ); ?>"
-	>
-
-	<?php if ( ! empty( $logo_search_link_url ) ) : ?>
-		</a><!-- /.logo -->
-	<?php else : ?>
-		</div><!-- /.logo -->
-	<?php endif; ?>
-
-<?php endif; ?>
+			<?php endif; ?>
+		</div>
+	</div>
+</div>
