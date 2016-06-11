2158215a698fb36c6d695de5589dd3cf9205ca4c
Wes Moberly <wesm87@gmail.com>
2016-06-11 05:37:34 -0400

    Automatically bump version number in Gulp `release` task

--- gulp-src/tasks/release.js
+++ gulp-src/tasks/release.js
@@ -5,6 +5,16 @@
 // Gulp
 const gulp = __require( 'gulp' );
 
+// Files
+const fs   = __require( 'fs' );
+
+// Process
+const cp   = __require( 'child_process' );
+const argv = __require( 'yargs' ).argv;
+
+// Utilities
+const semver = __require( 'semver' );
+
 // Tasks
 const build = __requireTask( 'build' );
 const docs  = __requireTask( 'docs' );
@@ -18,5 +28,51 @@ const docs  = __requireTask( 'docs' );
 export const task = 'release';
 
 
+/**
+ * Automatically bump the version number.
+ *
+ * @todo Update version number in `style.css`, `bower.json`, and `composer.json`.
+ */
+export const bump = () => {
+
+	const increment  = 1;
+	const errorCode  = 1;
+	const jsonSpaces = 2;
+	const pkg         = __require( './package.json' );
+	const versionInfo = {
+		major: semver.major( pkg.version ),
+		minor: semver.minor( pkg.version ),
+		patch: semver.patch( pkg.version ),
+	};
+
+	if ( '' !== cp.execSync( 'git status --porcelain' ).toString() ) {
+		console.error( 'Working dirty. Please commit before trying again' );
+		process.exit( errorCode );
+	}
+
+	if ( argv.major ) {
+		versionInfo.major += increment;
+		versionInfo.minor = 0;
+		versionInfo.patch = 0;
+	} else if ( argv.minor ) {
+		versionInfo.minor += increment;
+		versionInfo.patch = 0;
+	} else if ( argv.patch ) {
+		versionInfo.patch += increment;
+	}
+
+	const version = `${ versionInfo.major }.${ versionInfo.minor }.${ versionInfo.patch }`;
+
+	pkg.version = version;
+
+	const pkgJSON = JSON.stringify( pkg, null, jsonSpaces );
+
+	fs.writeFileSync( './package.json', `${ pkgJSON }\n` );
+
+	cp.execSync( `git commit -am "Release (${ version })"` );
+	cp.execSync( `git tag ${ version }` );
+};
+
+
 // Register the task.
-gulp.task( task, gulp.series( build.task, docs.task ) );
+gulp.task( task, gulp.series( build.task, docs.task, bump ) );
